b854a2e6f310918505a426c23799e198d3b72ddf
Filip Voska <filip.voska@infinum.com>
2025-10-02 22:31:17 +0200

    improve layout and menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'arw-root',
-	imports: [RouterOutlet, RouterLink, RouterLinkActive],
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		RouterLinkActive,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatIconModule,
+		MatButtonModule,
+		MatListModule,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
